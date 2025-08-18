4f257598e430dc4a605d054561e3cf2e48dcc454
Pooja Mankar <144476885+poojamankar30@users.noreply.github.com>
2025-08-18 17:05:58 +0530

    Create Problem2_WelcometoJava.java

---
+++ Problem2_WelcometoJava.java
@@ -0,0 +1,37 @@
+/*
+Input Format:- There is no input for this challenge.
+
+Output Format:- You must print two lines of output:
+
+Print Hello, World. on the first line.
+Print Hello, Java. on the second line.
+
+Sample Output
+
+Hello, World.
+Hello, Java.
+*/
+
+//CODE:-
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+       System.out.println("Hello, World.");
+       System.out.println("Hello, Java.");
+   }
+}
+
+//OUTPUT:-
+/*
+Your Output (stdout)
+Hello, World.
+Hello, Java.
+
+Expected Output
+Hello, World.
+Hello, Java.*/
