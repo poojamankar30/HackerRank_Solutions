95e8ad41a7af94f5c5a8e3a825a0910778c6d0dd
Pooja Mankar <144476885+poojamankar30@users.noreply.github.com>
2025-08-19 20:21:44 +0530

    Create Problem10_Int to String.java

---
+++ Problem10_Int to String.java
@@ -0,0 +1,41 @@
+/*
+You are given an integer n, you have to convert it into a string.
+
+Please complete the partially completed code in the editor. If your code successfully converts n into a string s the code will print "Good job". Otherwise it will print "Wrong answer".
+
+ n can range between -100 to 100 inclusive.
+
+Sample Input 0:
+100
+Sample Output 0:
+Good job
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args)throws NumberFormatException {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
+        Scanner sc= new Scanner(System.in);
+        int n= sc.nextInt();
+        String s= Integer.toString(n);
+        
+        if(n==Integer.parseInt(s)){
+            System.out.println("Good job");
+        }else{
+            System.out.println("Wrong answer");
+        }
+    }
+}
+
+//Output:
+/*
+Input (stdin)
+100
+Your Output (stdout)
+Good job
+Expected Output
+Good job*/
