f041907db8e0591e529a38b9c3cf4b36eaefc1b0
Pooja Mankar <144476885+poojamankar30@users.noreply.github.com>
2025-08-21 17:02:45 +0530

    Create Problem11_java Strings Introduction.java

---
+++ Problem11_java Strings Introduction.java
@@ -0,0 +1,67 @@
+/*
+Input Format
+
+The first line contains a string A. The second line contains another string B. The strings are comprised of only lowercase English letters.
+
+Output Format
+
+There are three lines of output:
+For the first line, sum the lengths of A and B.
+For the second line, write Yes if A is lexicographically greater than B otherwise print No instead.
+For the third line, capitalize the first letter in both A and B and print them on a single line, separated by a space.
+
+Sample Input 0
+
+hello
+java
+Sample Output 0
+
+9
+No
+Hello Java
+*/
+
+//Code: 
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+    
+        System.out.println(A.length()+B.length());
+        if(A.compareTo(B)>0){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+        String capA=A.substring(0,1).toUpperCase()+A.substring(1);
+        String capB=B.substring(0,1).toUpperCase()+B.substring(1);
+        
+        System.out.println(capA +" "+ capB);
+        
+    }
+}
+
+//OUTPUT:
+
+/*
+Input (stdin)
+hello
+java
+
+Your Output (stdout)
+9
+No
+Hello Java
+
+Expected Output
+9
+No
+Hello Java
+*/
